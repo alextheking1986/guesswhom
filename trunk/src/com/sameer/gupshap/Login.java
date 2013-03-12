177ac5f4b4c2ee9b843323280ba702df3bd297a2
syedsameer77@gmail.com <syedsameer77@gmail.com@2fe6f1b1-a229-f117-0869-abe86bf895ad>
2013-03-12 16:12:03 +0000

    First entry of my project

---
+++ trunk/src/com/sameer/gupshap/Login.java
@@ -0,0 +1,23 @@
+package com.sameer.gupshap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
